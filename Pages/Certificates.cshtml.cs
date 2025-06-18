4e967682c6c5a4a9025dbbe435c5f10277b2bf65
Patrik Pospíšil <pospisil.patrik20@gmail.com>
2025-06-18 23:48:04 +0200

    Záloha před přidáním Bootstrap šablony Personal

---
+++ Pages/Certificates.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Patrik_Pospisil_Portfolio_Web.Pages
+{
+    public class CertificatesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
